ed6f2d7f4fa644f3d8d5cf01f70973b0bfa3aacf
AnDrZin22 <conteandre19@gmail.com>
2024-10-24 14:17:56 -0300

    Update comprovante.php

--- comprovante.php
+++ comprovante.php
@@ -1,21 +1,44 @@
 <?php
+// Define prices
+$prices = [
+    "opcao1" => 9.50,  // X-Burguer
+    "opcao2" => 13.50, // X-Salada
+    "opcao3" => 17.50, // X-Egg Duplo
+    "bebida1" => 6.50, // Coca Cola Lata
+    "bebida2" => 9.00, // Cerveja Heineken
+    "bebida3" => 4.50   // Limonada
+];
 
+// Initialize total cost
+$total = 0;
+$orderDetails = [];
 
-$quantidade1 = $_POST["quantidade1"];// xburguer
-$quantidade2 = $_POST["quantidade2"];// misto quente
-$quantidade3 = $_POST["quantidade3"];// x egg
-$quantidade4 = $_POST["quantidade4"];// coca cola
-$quantidade5 = $_POST["quantidade5"];// heineken
-$quantidade6 = $_POST["quantidade6"];//limonada
-$obs = $_POST["obs"]; //obs
-$ValorTotal = 0.00;
-
-if (isset($_POST["opcao6"])) {
-    $ValorTotal = 5.50 * $quantidade6;
+// Process sandwich orders
+foreach (["opcao1", "opcao2", "opcao3"] as $option) {
+    if (isset($_POST[$option]) && $_POST[$option] == "on") {
+        $quantity = (int)$_POST["quantidade" . substr($option, -1)];
+        if ($quantity > 0) {
+            $cost = $prices[$option] * $quantity;
+            $total += $cost;
+            $orderDetails[] = ucfirst($option) . " - Quantidade: $quantity - Valor: R$" . number_format($cost, 2, ',', '.');
+        }
+    }
 }
-    echo "pedido realizado com sucesso!<br>";
-    echo "Sua conta ficou em R$: ", $ValorTotal;
-    echo "Obs ", $obs;
-    echo "<br><a href= 'Cardápio.html'>Voltar</a>";
-    ?>
 
+// Process drink orders
+foreach (["bebida1", "bebida2", "bebida3"] as $option) {
+    if (isset($_POST[$option]) && $_POST[$option] == "on") {
+        $quantity = (int)$_POST["quantidade_" . substr($option, -1)];
+        if ($quantity > 0) {
+            $cost = $prices[$option] * $quantity;
+            $total += $cost;
+            $orderDetails[] = ucfirst($option) . " - Quantidade: $quantity - Valor: R$" . number_format($cost, 2, ',', '.');
+        }
+    }
+}
+
+// Get observations
+$observations = isset($_POST['obs']) ? htmlspecialchars($_POST['obs']) : "Nenhuma observação.";
+
+// HTML output
+?>
